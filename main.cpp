a19a16c555ac870976cafa7c549f10569c4fbcc0
Pavel Gerasin <374007@gmail.com>
2022-11-03 19:48:42 +0700

    02.11.2022

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-std::string normal = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm!#$%&*+-'\/=?^_`{|}~";
+std::string simbol_first = "1234567890QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm!#$%&*+-'./=?^_`{|}~\\";
+std::string simbol_second = "1234567890QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm.-";
 bool valid=true;
 
 std::string first (std::string first_part)
@@ -15,19 +16,18 @@ std::string second (std::string second_part)
     return second_part;
 }
 
-bool simbol (std::string text) {
+bool simbol (std::string text, std::string dictionary) {
    bool result = true;
     for (int i = 0; i < text.length();i++) {
-       // for (int j = 0; j < normal.length(); j++) {
-            if (normal.find(text[i]) == -1)  {
-               result = false;
-               break ;
-            }
-       // }
+       if (dictionary.find(text[i]) == -1)  {
+          result = false;
+          break ;
+       }
+
     }
     return result;
-
 }
+
 int main() {
     std::string email, first_part, second_part;
     std::cout << "Email Verify!" << std::endl;
@@ -35,27 +35,22 @@ int main() {
     std::cin >> email;
     first_part = first (email);
     second_part =  second (email);
-    if (first_part.length()+second_part.length()+1 == email.length()  && (first_part.length()>=1 && first_part.length()<=64) &&
-       (second_part.length()>=1 && second_part.length()<=63)) {
+    if (first_part.length() + second_part.length() + 1 == email.length()  && (first_part.length() >= 1 && first_part.length() <= 64) &&
+       (second_part.length() >= 1 && second_part.length() <= 63)) {
 
         if  (first_part.find("..") != -1)  valid = false;
-      std::cout << first_part.find("..") << "\n";
-        valid = simbol (first_part) ;
+        if (!simbol (first_part,simbol_first))  valid = false;
+        if (!simbol (second_part,simbol_second)) valid = false;
+        if (first_part[0] == '.' ) valid = false;
+        if (second_part[second_part.length()-1] == '.') valid = false;
     }
     else {
         valid = false;
-        std::cout << "Error!!!!" << "\n";
     }
-
-
     if (!valid) {
-        std::cout << "ERROR" << "\n";}
+        std::cout << "NO!" << "\n";}
     else {
-        std::cout << "VALID"<< "\n";
+        std::cout << "YES!"<< "\n";
     }
-    std::cout << first (email) << "\n";
-    std::cout << second (email) << "\n";
-    std::cout << valid << "\n";
 
-    return 0;
 }
